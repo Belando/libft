8a0e609bb3ce25764a677e921574dedfad877475
Francisco Belando Carrion <fbelando@c2r4s3.42madrid.com>
2023-09-23 17:53:39 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -12,35 +12,88 @@
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+typedef struct s_data
 {
-	size_t	count;
+	char	**result;
 	size_t	i;
 	size_t	j;
 	size_t	start;
-	char	**result;
+}		t_data;
+
+static void	ft_free(char **str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		free(str[i]);
+		i++;
+	}
+	free (str);
+}
+
+static size_t	ft_count(const char *s, char c)
+{
+	size_t	count;
+	size_t	i;
 
 	count = 0;
 	i = 0;
-	if (s == NULL)
-		return (NULL);
 	while (s[i])
 	{
-		if (s[i] == c)
-			i++;
-		else
+		if (s[i] != c)
 		{
-			start = i;
+			count++;
 			while (s[i] && s[i] != c)
 				i++;
-			result = (char **)realloc((result, count + 2) * sizeof(char *));
-			result[count] = (char *)malloc((i - start + 1) * sizeof(char));
-			while (start < i)
-				result[count][j++] = s[start++];
-			result[count++][j] = '\0';
-			j = 0;
 		}
+		else
+			i++;
 	}
-	result[count] = NULL;
-	return (result);
+	return (count);
+}
+
+static char	*ft_extract(const char *s, size_t start, size_t end)
+{
+	char	*substring;
+	size_t	j;
+
+	j = 0;
+	substring = (char *)malloc((end - start + 1) * sizeof(char));
+	if (substring == NULL)
+		return (NULL);
+	while (start < end)
+		substring[j++] = s[start++];
+	substring[j] = '\0';
+	return (substring);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	t_data	vars;
+
+	vars.i = 0;
+	vars.j = 0;
+	vars.start = 0;
+	vars.result = (char **)malloc((ft_count(s, c) + 1) * sizeof(char *));
+	if (vars.result == NULL)
+		return (NULL);
+	while (s[vars.i])
+	{
+		if (s[vars.i] != c)
+		{
+			vars.start = vars.i;
+			while (s[vars.i] && s[vars.i] != c)
+				vars.i++;
+			vars.result[vars.j] = ft_extract(s, vars.start, vars.i);
+			if (vars.result[vars.j] == NULL)
+				return (ft_free(vars.result), NULL);
+			vars.j++;
+		}
+		else
+			vars.i++;
+	}
+	vars.result[vars.j] = NULL;
+	return (vars.result);
 }
